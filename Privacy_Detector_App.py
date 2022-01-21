5c542975c93eef62bb9b49c85d579e00e12217de
Francesco Casillo <casillofran95@gmail.com>
2022-01-21 16:00:29 +0100

    Uploaded models, components and requirements

---
+++ Privacy_Detector_App.py
@@ -0,0 +1,187 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from pathlib import Path
+from string import punctuation
+from collections import Counter
+from nltk.corpus import stopwords
+
+import spacy
+from spacy import displacy
+from IPython.core.display import display, HTML
+
+import numpy as np
+
+from keras.models import Model, Sequential
+from keras import models
+from tensorflow import keras
+import pickle
+import pandas as pd
+import html
+import string
+from Privacy_Dictionary.liwc_class import Liwc
+
+def extract_features(user_story):
+    PERSONS = ['I', 'ME', 'MY', 'MINE', 'YOU', 'YOUR', 'YOURS', 'HE', 'SHE', 
+           'HIS', 'HER', 'HIM', 'THEY', 'THEM', 'THEMSELVES', 'OUR', 'WE']
+    LOCATION = ['LOC', 'GPE', 'ORG', 'FAC', 'CARDINAL']
+    ALL_ENTITIES = LOCATION + ['PERSON', 'HEALTH', 'MONEY', 'DATE', 'TIME']
+    
+    doc = nlp(u''+user_story)
+    
+    text_tokenized = []
+    modified_tokens = []
+    modified_tokens_dep = []
+    modified_tokens_pos = []
+        
+    for t in doc:
+        if str(t) not in punctuation:
+            if t.ent_type_ == '':
+                if t.text.upper() in PERSONS:
+                    modified_tokens.append('PERSON')
+                    text_tokenized.append(t.text)
+                else:
+                    modified_tokens.append(t.text)
+                    text_tokenized.append(t.text)
+            else:
+                modified_tokens.append(t.ent_type_)
+                text_tokenized.append(t.text)
+    
+    for t in doc:
+        if str(t) not in punctuation:
+            modified_tokens_dep.append(t.dep_)
+ 
+    for t in doc:
+        if str(t) not in punctuation:
+            modified_tokens_pos.append(t.pos_)
+    
+    c,keywords = dictionary.parse(user_story.lower().split(' '))
+    categories_list = [list(i) for i in c.items()]
+    if str(keywords) == "[]":
+        keywords = "none"
+        categories_list = "none"
+
+    return modified_tokens, modified_tokens_dep, modified_tokens_pos, categories_list, keywords, text_tokenized
+
+
+def prepare_input_privacy(sentence):
+    
+    max_length=558
+    output = disclo_cnn.layers[-4].output
+    disclo_cnn_cutted = Model(disclo_cnn.input, output)
+
+    disclo_cnn_cutted.trainable = False
+    for layer in disclo_cnn_cutted.layers:
+        layer.trainable = False
+    modified_tokens, modified_tokens_dep, modified_tokens_pos, counter_list, k, k_grams = extract_features(sentence)
+    
+
+    encoded1 = tokenizer.texts_to_sequences(modified_tokens[:])
+    encoded2 = tokenizer.texts_to_sequences(modified_tokens_dep[:])
+    encoded3 = tokenizer.texts_to_sequences(modified_tokens_pos[:])
+    
+    encoded1_x = np.zeros((1,max_length))
+    count=0
+    for i in encoded1:
+        if len(i) >0:
+            encoded1_x[0,count]=i[0]
+        count+=1
+
+    encoded2_x = np.zeros((1,max_length))
+    count=0
+    for i in encoded2:
+        if len(i) >0:
+            encoded2_x[0,count]=i[0]
+        count+=1
+
+    encoded3_x = np.zeros((1,max_length))
+    count=0
+    for i in encoded3:
+        if len(i) >0:
+            encoded3_x[0,count]=i[0]
+        count+=1
+    encoded4_x = np.zeros((1,max_length))
+    count=0
+    for i in tokenizer.texts_to_sequences(str(counter_list)):
+        if len(i) >0:
+            encoded4_x[0,count]=i[0]
+        count+=1
+    encoded5_x = np.zeros((1,max_length))
+    count=0
+    for i in tokenizer.texts_to_sequences(str(k)):
+        if len(i) >0:
+            encoded5_x[0,count]=i[0]
+        count+=1
+    output_disclo = disclo_cnn_cutted.predict([encoded1_x,encoded2_x,encoded3_x])
+    df_coeff = pd.DataFrame(
+    {'word': k_grams
+    })
+    
+    
+    return encoded4_x, encoded5_x, output_disclo, df_coeff, k
+
+def weights_to_HTML(df_coeff, keywords):
+    #Highlight for privacy words
+    highlighted_text4 = pd.DataFrame(columns=["token"])
+    count=0
+    for word in df_coeff['word']:
+        if count<(len(df_coeff['word'])):
+    
+            if ((word in keywords) & (word not in string.punctuation)):
+                highlighted_text4.loc[count] = '<span style="background-color:rgba(207, 0, 15,0.8);color:#000000;">' + html.escape(word) + '</span>'
+            else:
+                highlighted_text4.loc[count] = '<span style="color:#000000;">' + word + '</span>'
+    
+        count+=1
+    
+    return highlighted_text4
+
+app = Flask(__name__)
+# Configure a secret SECRET_KEY
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+
+# LOAD THE MODELS, DICTIONARY AND TRANSFORMERS!
+dictionary = Liwc('Privacy_Dictionary/privacydictionary_TAWC.dic')
+disclo_cnn = keras.models.load_model('Disclosure_CNN/cnn.h5')
+with open('Disclosure_CNN/transformer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+nlp = spacy.load('Disclosure_NER')
+privacy_detector = keras.models.load_model('Privacy_Detector_TL/privacy_detector.h5')
+
+user_story = ''
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    return render_template('home.html')
+
+
+@app.route('/prediction',methods = ['POST'])
+def prediction():
+    
+    global user_story
+    
+    user_story = str(request.form['user_story'])
+    input1,input2,input3, df_coeff, keywords= prepare_input_privacy(user_story)
+
+    prediction = privacy_detector.predict([input1,input2,input3])
+    
+    doc = nlp(user_story)
+    
+    x1 = displacy.render(doc, style='ent', page=True)
+    output_path = Path("./static/entities.html")
+    output_path.open("w", encoding="utf-8").write(x1)
+    
+    x2 = displacy.render(doc, style='dep', page=True)
+    output_path = Path("./static/dep_tree.html")
+    output_path.open("w", encoding="utf-8").write(x2)
+    
+    explain4 = weights_to_HTML(df_coeff, keywords)
+    
+
+    return render_template('prediction.html',result = prediction,graph1 = x1,graph2 = x2, explain4 = explain4)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Privacy_Detector
+# PReDUS: A Privacy Requirements Detector from User Stories
+
+PReDUS is a web application that want to provide some insights about privacy requirements.
+
+The tool is highly recommended for stakeholders with little experience in the identification of privacy content.
+
+The algorithm behind PReDUS requires an US as input, whose format is as the following:
+```
+As a project manager I want to access to data about my colleagues progress so I can better report our success and failures.
+```
+The output of this application is the prevision of the deep learning algorithm developed to detect privacy content, supported by some information about the words higly related to privacy matters and which kind of privacy they're related to.
+
+## Installation & Configuration
+
+In order to run the web application, all you need is Python.
+
+The application has been tested till 3.9.9 Python version. You can download it [here](https://www.python.org/downloads/).
+
+Once Python is installed, you need to install all the packages listed into ```requirements.txt``` file. You can do it with the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+Or install the packages one at time. Example:
+
+```bash
+pip install pandas
+```
+
+Please, be carefull with version of some packages, or you'll have some issues by using the tool.
+
+Once all the packages are installed, run ```Privacy_Detector_App.py``` script to use the tool. You can do it by using the following command:
+
+```bash
+python Privacy_Detector_App.py
+```
+
+Once the script has ended you'll get a result like the following one:
+```bash
+ * Debugger is active!
+ * Debugger PIN: XXX-XXX-XXX
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+```
+
+Open your browser and type the url provided. (In the example: http://127.0.0.1:5000/)
